ddd4ceac4ea5afdfeea50e8cac1435743455dc53
proXDhiya <prox.dhiya@gmail.com>
2022-08-24 19:49:25 +0100

    update responsive

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -6,13 +6,19 @@ let state = false;
 mobileNavButton.addEventListener('click', () => {
     if (state === false) {
         slider.style.left = '0';
+        slider.style.boxShadow = 'rgba(81, 82, 87, 0.2) 0px 5px 15px 1200px';
         logo[1].style.color = '#fff';
         mobileNavButton.name = 'close';
+        mobileNav.style.backgroundColor = 'transparent';
+        mobileNav.style.boxShadow = 'none';
         state = true;
     } else {
         slider.style.left = '-85%';
+        slider.style.boxShadow = 'none';
         logo[1].style.color = 'transparent';
         mobileNavButton.name = 'menu';
+        mobileNav.style.backgroundColor = 'var(--background-color)';
+        mobileNav.style.boxShadow = 'rgba(0, 0, 0, 0.45) 0px 15px 20px -20px';
         state = false;
     }
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,20 @@
 const nav = document.querySelector('nav');
+const mobileNav = document.querySelector('.mobile-nav');
 
 window.onscroll = () => {
-    var distanceY = document.documentElement.scrollTop;
-    if (distanceY > 50)
+    let distanceY = document.documentElement.scrollTop;
+    if (distanceY > 50 && !state) {
+        // mobile
+        mobileNav.classList.add('nav-scrolled');
+        mobileNav.style.boxShadow = 'rgba(0, 0, 0, 0.45) 0px 15px 20px -20px';
+        // desktop
         nav.classList.add('nav-scrolled');
-    else
+    }
+    else {
+        // mobile
+        mobileNav.classList.remove('nav-scrolled');
+        mobileNav.style.boxShadow = 'none';
+        // desktop
         nav.classList.remove('nav-scrolled');
+    }
 }
