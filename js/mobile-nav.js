b4e32e9532d5e1486ed616714992b0e57334b85c
proXDhiya <prox.dhiya@gmail.com>
2022-08-26 09:53:09 +0100

    UPDAAAAAAAAAAAAAAAAAATE

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,4 +1,5 @@
 const mobileNavButton = document.querySelector('.mobile-nav > ion-icon');
+const mobileNavElements = document.querySelectorAll('.mobile-slider>ul>li>a');
 const logo = document.querySelectorAll('.logo-name');
 const slider = document.querySelector('.mobile-slider');
 let state = false;
@@ -23,4 +24,17 @@ mobileNavButton.addEventListener('click', () => {
         mobileNav.style.boxShadow = 'rgba(0, 0, 0, 0.45) 0px 15px 20px -20px';
         state = false;
     }
+});
+
+mobileNavElements.forEach(element => {
+    element.addEventListener('click', () => {
+        slider.style.left = '-85%';
+        slider.style.boxShadow = 'none';
+        logo[1].style.color = 'transparent';
+        mobileNavButton.name = 'menu';
+        mobileNavButton.style.color = 'inherit';
+        mobileNav.style.backgroundColor = 'var(--background-color)';
+        mobileNav.style.boxShadow = 'rgba(0, 0, 0, 0.45) 0px 15px 20px -20px';
+        state = false;
+    } );
 });
