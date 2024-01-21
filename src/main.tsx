1cf51c76747582f01ed0e424f2c67145014afd45
Dhiya Kellouche <prox.dhiya@gmail.com>
2024-01-21 19:56:08 +0000

    add chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
+import theme from './theme/theme.ts'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>
 )
